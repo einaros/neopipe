593097f73022f470e48023f51cf891d0e0d64e7e
einaros <einaros@vg.no>
2018-08-16 12:23:14 +0200

    eliminate code repeat for stdin/single argument execution, improve error output

--- neopipe.js
+++ neopipe.js
@@ -18,17 +18,51 @@ const stringReplaceAsync = require('string-replace-async');
 const promiseLimit = require('promise-limit');
 const sha1 = require('sha1');
 const os = require('os');
+const EventEmitter = require('events');
 
 const NS_PER_SEC = 1e9;
 const compiledGrammar = nearley.Grammar.fromCompiled(grammar);
 const compiledSubgrammar = nearley.Grammar.fromCompiled(subgrammar);
 const exec = util.promisify(require('child_process').exec);
 const cpuCount = os.cpus().length;
+const timeStarted = process.hrtime();
+
+program
+  .version(require('./package.json').version)
+  .description(require('./package.json').description)
+  .option('-b, --keep-blank', 'Keep expressions with blank ids')
+  .option('-s, --separator [separator]', 'Custom field separator for stdin interpolation.', ' ')
+  .option('-q, --quote [quote]', 'Custom quote char for stdin interpolation.', '"')
+  .option('-p, --pipe', 'Pipe input to output.')
+  .option('-j, --jobs [jobs]', 'Limit number of concurrent jobs.', cpuCount)
+  .option('-i, --pipe-interpolated', 'Pipe interpolated input to output.')
+  .option('-t, --testonly', 'Simulate insertion.')
+  .option('-v, --verbose', 'Increase verbosity.', (v, total) => total + 1, 0)
+  .option('--neohost [host]', 'Neo4j hostname', 'localhost')
+  .option('--neouser [user]', 'Neo4j username', null)
+  .option('--neopasswd [passwd]', 'Neo4j password', null)
+  .on('--help', function(){ console.log(require('./help.js')); })
+  .parse(process.argv);
+
+const instructionTemplate = program.args.join(' ').trim();
+if (instructionTemplate == '') {
+  program.help();
+  process.exit(1);
+}
+
+if (program.testonly) console.error('* Simulation starting - will not commit to Neo4j'.cyan);
+const [driver, session, tx] = establishConnection();
 
 function establishConnection() {
-  const driver = neo4j.driver('bolt://localhost');
+  if (program.verbose > 0) console.error(`* Connecting to Neo4j @ ${program.neohost}`.cyan);
+  let auth;
+  if (program.neouser) {
+    auth = neo4j.auth.basic(program.neouser, program.neopasswd);
+  }
+  const driver = neo4j.driver(`bolt://${program.neohost}`, auth);
   const session = driver.session();
-  return [driver, session]
+  const tx = session.beginTransaction();
+  return [driver, session, tx];
 }
 
 function unesc(s) {
@@ -72,6 +106,10 @@ function processInstruction(session, instructionLine) {
   const parser = new nearley.Parser(compiledGrammar);
   parser.feed(instructionLine);
   let instruction = parser.results[0];
+
+  if (!instruction) {
+    throw new Error(`Expression parsing failed: ${instructionLine}`);
+  }
 
   let leftName = instruction.left.name;
   let leftMergeOn = instruction.left.mergeOn || 'id';
@@ -162,7 +200,7 @@ async function processStdinLine(line) {
   return new Promise(async (accept, reject) => {
     if (program.pipe && !program.pipeInterpolated) console.log(line);
     if (program.verbose > 0) {
-      console.error(`> ${line}`.yellow);
+      console.error(`> ${line}`.white);
     }
     line = parse(line, { delimiter: program.separator, quote: program.quote })[0];
     // interpolate stdin lines
@@ -213,52 +251,39 @@ async function processStdinLine(line) {
   });
 }
 
-program
-  .version(require('./package.json').version)
-  .description(require('./package.json').description)
-  .option('-b, --keep-blank', 'Keep expressions with blank ids')
-  .option('-s, --separator [separator]', 'Custom field separator for stdin interpolation.', ' ')
-  .option('-q, --quote [quote]', 'Custom quote char for stdin interpolation.', '"')
-  .option('-p, --pipe', 'Pipe input to output.')
-  .option('-j, --jobs [jobs]', 'Limit number of concurrent jobs.', cpuCount)
-  .option('-i, --pipe-interpolated', 'Pipe interpolated input to output.')
-  .option('-t, --testonly', 'Simulate insertion.')
-  .option('-v, --verbose', 'Increase verbosity.', (v, total) => total + 1, 0)
-  .on('--help', function(){ console.log(require('./help.js')); })
-  .parse(process.argv);
 
-const instructionTemplate = program.args.join(' ').trim();
-if (instructionTemplate == '') {
-  program.help();
-  process.exit(1);
+function processPendingInserts(pendingNeoPromises) {
+  let exitCode = 0;
+  Promise.all(pendingNeoPromises)
+    .then((results) => {
+      results = results.filter(r => r);
+      // Detect errors
+      for (let result of results) {
+        if (result.error || result.message) throw result; 
+      }
+      // Presumably no errors, try commiting
+      return tx.commit()
+        .then(() => {
+          const timeDiff = process.hrtime(timeStarted);
+          const timeElapsed = (timeDiff[0] * NS_PER_SEC + timeDiff[1]) / NS_PER_SEC;
+          if (program.testonly) console.error(`* ${results.length} expressions simulated (${timeElapsed.toFixed(2)} sec).`.cyan);
+          else console.error(`* ${results.length} expressions inserted into Neo4j (${timeElapsed.toFixed(2)} sec).`.cyan);
+        });
+    })
+    .catch((e) => {
+      console.log(`* Error(s) during Neo4j operation: ${e.message || e.error}`.red)
+      console.log(`* In the case of dubious errors, check that the database is running and that connection details are correct.`.yellow)
+      exitCode = 1;
+    })
+    .finally(() => {
+      session.close();
+      driver.close();
+      process.exit(exitCode);
+    });
 }
 
-const timeStarted = process.hrtime();
-if (program.testonly) console.error('* Simulation starting - will not commit to Neo4j'.cyan);
-
-const limit = promiseLimit(program.jobs);
-
-if (process.stdin.isTTY) {
-  // Process single instruction from the command line
-  const [driver, session] = establishConnection();
-  let inserted = processInstruction(session, instructionTemplate);
-  if (!inserted) {
-    session.close();
-    driver.close();
-    process.exit();
-  }
-  inserted.then(() => {
-    session.close();
-    driver.close();
-    const timeDiff = process.hrtime(timeStarted);
-    const timeElapsed = (timeDiff[0] * NS_PER_SEC + timeDiff[1]) / NS_PER_SEC;
-    if (program.testonly) console.error(`* Single expression simulated (${timeElapsed.toFixed(2)} sec).`.cyan);
-    else console.error(`* Single expression inserted into Neo4j (${timeElapsed.toFixed(2)} sec).`.cyan);
-  });
-}
-else {
+if (!process.stdin.isTTY) {
   // Process lines from stdin interpolated with an instruction from the command line
-  const timeout = ms => new Promise(res => setTimeout(res, ms))
   let readline = require('readline');
   let rl = readline.createInterface({
     input: process.stdin,
@@ -266,25 +291,19 @@ else {
     terminal: false
   });
   if (program.verbose > 0) console.error(`* Collecting input from stdin.`.cyan);
-  let pendingInput = [];
-  const [driver, session] = establishConnection();
+  const limit = promiseLimit(program.jobs);
+  let pendingNeoPromises = [];
   rl.on('line', async (line) => {
-    pendingInput.push(
-            limit(() => processStdinLine(line))
-      .then(limit(v => processInstruction(session, v))));
+    pendingNeoPromises.push(
+      limit(() => processStdinLine(line))
+        .then(v => processInstruction(tx, v))
+        .catch(e => e)
+    );
   });
-  rl.on('close', () => {
-    Promise.all(pendingInput).then((results) => {
-      let neoPromises = results.filter(i => i); // only awit for instructions that resulted in neo inserts
-      Promise.all(neoPromises).then(() => {
-        session.close();
-        driver.close();
-        const timeDiff = process.hrtime(timeStarted);
-        const timeElapsed = (timeDiff[0] * NS_PER_SEC + timeDiff[1]) / NS_PER_SEC;
-        if (program.testonly) console.error(`* ${neoPromises.length} expressions simulated (${timeElapsed.toFixed(2)} sec).`.cyan);
-        else console.error(`* ${neoPromises.length} expressions inserted into Neo4j (${timeElapsed.toFixed(2)} sec).`.cyan);
-      });
-    });
-  });
+  rl.on('close', () => processPendingInserts(pendingNeoPromises));
+}
+else {
+  // Process single instruction from the command line
+  processPendingInserts([Promise.resolve().then(() => processInstruction(tx, instructionTemplate))]);
 }
 
